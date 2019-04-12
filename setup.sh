8e419178fe14fbc306887f7a9c7309fb5d1e9846
Amir Shahatit <ashahatit@berkeley.edu>
2019-04-12 15:26:22 -0700

    setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,6 @@ tmp_dir=$(mktemp -d -t ci-XXXXXXXXXX)
 cd $tmp_dir
 git clone https://github.com/amir656/btExperiment.git
 cd btExperiment
-mv ../../torrents btExperiment/torrents
-mv ../../*.sh btExperiment/
+mv ~/torrents btExperiment/torrents
+mv ~/*.sh btExperiment/
 sudo docker build -t kraken .
